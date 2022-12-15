3ae0491f4110447b398ead2f04859ac3674838ea
moe380183 <105432882+moe380183@users.noreply.github.com>
2022-12-15 16:53:17 +0000

    Update PledgeFactory.sol
    Added the Chain ID parmeter

--- PledgeFactory.sol
+++ PledgeFactory.sol
@@ -2,20 +2,23 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
+/**
+ * @title Storage
+ * @dev Store & retrieve value in a variable
+ * @custom:dev-run-script ./scripts/deploy_with_ethers.ts
+ */
 import "./Pledge.sol";
 
 contract PledgeFactory {
     address[] internal addresses;
 
-    function makePledge(string memory pledgeString)
+    function makePledge(string memory pledgeString, uint chainId)
         public
-        payable
-        returns (address)
+        payable        
     {
         Pledge pledge;
-        pledge = new Pledge(pledgeString);
-        addresses.push(address(pledge));
-        return address(pledge);
+        pledge = new Pledge(pledgeString, chainId);
+        addresses.push(address(pledge));        
     }
 
     function getPledgeAddresses() public view returns (address[] memory) {
